a6e1edeff35a68b394dc1b37c3bb0368013b225f
cauamp <cauamp11@gmail.com>
2026-02-10 18:20:28 -0700

    update paths

---
+++ job.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --time=00:5:00          # Time limit (HH:MM:SS) 
+#SBATCH --output=result-%j.out   # Output file name 
+#SBATCH --error=gpu_test_%j.err
+#SBATCH --job-name=caua_qwen_test_job   # Name of the job
+
+#SBATCH --account=def-vislearn
+
+#SBATCH --mem=128G                 # Request 128GB of memory
+#SBATCH --cpus-per-task=4        # Request CPU core
+#SBATCH --gpus-per-node=1         # Request GPU per node
+
+# 1. Load the same modules you used to create the env
+module load python/3.11
+
+# 2. Activate your virtual environment
+# source /home/caua/projects/def-vislearn/cauamp/qwen/bin/activate
+conda activate dvsod
+
+# 3. Run your python script
+python ~/projects/def-vislearn/caua/DVSOD-Baseline/train.py
