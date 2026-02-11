47189f09b49efac6d0a23a6798bbdf4de1c8acc0
cauamp <cauamp11@gmail.com>
2026-02-10 20:54:12 -0700

    refatorar código para melhorar a legibilidade e consistência na formatação

--- model/CPD_ResNet_models.py
+++ model/CPD_ResNet_models.py
@@ -10,9 +10,15 @@ from model.ResNet import B2_ResNet
 class BasicConv2d(nn.Module):
     def __init__(self, in_planes, out_planes, kernel_size, stride=1, padding=0, dilation=1):
         super(BasicConv2d, self).__init__()
-        self.conv = nn.Conv2d(in_planes, out_planes,
-                              kernel_size=kernel_size, stride=stride,
-                              padding=padding, dilation=dilation, bias=False)
+        self.conv = nn.Conv2d(
+            in_planes,
+            out_planes,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+            dilation=dilation,
+            bias=False,
+        )
         self.bn = nn.BatchNorm2d(out_planes)
         self.relu = nn.ReLU(inplace=True)
 
@@ -34,21 +40,21 @@ class RFB(nn.Module):
             BasicConv2d(in_channel, out_channel, 1),
             BasicConv2d(out_channel, out_channel, kernel_size=(1, 3), padding=(0, 1)),
             BasicConv2d(out_channel, out_channel, kernel_size=(3, 1), padding=(1, 0)),
-            BasicConv2d(out_channel, out_channel, 3, padding=3, dilation=3)
+            BasicConv2d(out_channel, out_channel, 3, padding=3, dilation=3),
         )
         self.branch2 = nn.Sequential(
             BasicConv2d(in_channel, out_channel, 1),
             BasicConv2d(out_channel, out_channel, kernel_size=(1, 5), padding=(0, 2)),
             BasicConv2d(out_channel, out_channel, kernel_size=(5, 1), padding=(2, 0)),
-            BasicConv2d(out_channel, out_channel, 3, padding=5, dilation=5)
+            BasicConv2d(out_channel, out_channel, 3, padding=5, dilation=5),
         )
         self.branch3 = nn.Sequential(
             BasicConv2d(in_channel, out_channel, 1),
             BasicConv2d(out_channel, out_channel, kernel_size=(1, 7), padding=(0, 3)),
             BasicConv2d(out_channel, out_channel, kernel_size=(7, 1), padding=(3, 0)),
-            BasicConv2d(out_channel, out_channel, 3, padding=7, dilation=7)
+            BasicConv2d(out_channel, out_channel, 3, padding=7, dilation=7),
         )
-        self.conv_cat = BasicConv2d(4*out_channel, out_channel, 3, padding=1)
+        self.conv_cat = BasicConv2d(4 * out_channel, out_channel, 3, padding=1)
         self.conv_res = BasicConv2d(in_channel, out_channel, 1)
 
     def forward(self, x):
@@ -70,24 +76,23 @@ class aggregation(nn.Module):
         super(aggregation, self).__init__()
         self.relu = nn.ReLU(True)
 
-        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        self.upsample = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True)
         self.conv_upsample1 = BasicConv2d(channel, channel, 3, padding=1)
         self.conv_upsample2 = BasicConv2d(channel, channel, 3, padding=1)
         self.conv_upsample3 = BasicConv2d(channel, channel, 3, padding=1)
         self.conv_upsample4 = BasicConv2d(channel, channel, 3, padding=1)
-        self.conv_upsample5 = BasicConv2d(2*channel, 2*channel, 3, padding=1)
+        self.conv_upsample5 = BasicConv2d(2 * channel, 2 * channel, 3, padding=1)
 
-        self.conv_concat2 = BasicConv2d(2*channel, 2*channel, 3, padding=1)
-        self.conv_concat3 = BasicConv2d(3*channel, 3*channel, 3, padding=1)
-        self.conv4 = BasicConv2d(3*channel, 3*channel, 3, padding=1)
+        self.conv_concat2 = BasicConv2d(2 * channel, 2 * channel, 3, padding=1)
+        self.conv_concat3 = BasicConv2d(3 * channel, 3 * channel, 3, padding=1)
+        self.conv4 = BasicConv2d(3 * channel, 3 * channel, 3, padding=1)
         self.conv4_2 = BasicConv2d(3 * channel, 2 * channel, 3, padding=1)
-        self.conv5 = nn.Conv2d(2*channel, 1, 1)
+        self.conv5 = nn.Conv2d(2 * channel, 1, 1)
 
     def forward(self, x1, x2, x3):
         x1_1 = x1
         x2_1 = self.conv_upsample1(self.upsample(x1)) * x2
-        x3_1 = self.conv_upsample2(self.upsample(self.upsample(x1))) \
-               * self.conv_upsample3(self.upsample(x2)) * x3
+        x3_1 = self.conv_upsample2(self.upsample(self.upsample(x1))) * self.conv_upsample3(self.upsample(x2)) * x3
 
         x2_2 = torch.cat((x2_1, self.conv_upsample4(self.upsample(x1_1))), 1)
         x2_2 = self.conv_concat2(x2_2)
@@ -116,9 +121,9 @@ class CPD_ResNet(nn.Module):
         self.rfb3_2 = RFB(1024, channel)
         self.rfb4_2 = RFB(2048, channel)
         self.agg2 = aggregation(channel)
-        self.up8 = nn.Upsample(scale_factor=8, mode='bilinear', align_corners=True)
-        self.up4 = nn.Upsample(scale_factor=4, mode='bilinear', align_corners=True)
-        self.up2 = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        self.up8 = nn.Upsample(scale_factor=8, mode="bilinear", align_corners=True)
+        self.up4 = nn.Upsample(scale_factor=4, mode="bilinear", align_corners=True)
+        self.up2 = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True)
 
         self.HA = HA()
         if self.training:
@@ -151,19 +156,22 @@ class CPD_ResNet(nn.Module):
         return self.up8(attention_map), self.up4(detection_map), x2_2, x3_2, x4_2
 
     def initialize_weights(self):
-        res50 = models.resnet50(pretrained=True)
+        res50 = models.resnet50(weights=None)
+        weights_path = "./resnet50-0676ba61.pth"
+        state_dict = torch.load(weights_path)
+        res50.load_state_dict(state_dict)
         pretrained_dict = res50.state_dict()
         all_params = {}
         for k, v in self.resnet.state_dict().items():
             if k in pretrained_dict.keys():
                 v = pretrained_dict[k]
                 all_params[k] = v
-            elif '_1' in k:
-                name = k.split('_1')[0] + k.split('_1')[1]
+            elif "_1" in k:
+                name = k.split("_1")[0] + k.split("_1")[1]
                 v = pretrained_dict[name]
                 all_params[k] = v
-            elif '_2' in k:
-                name = k.split('_2')[0] + k.split('_2')[1]
+            elif "_2" in k:
+                name = k.split("_2")[0] + k.split("_2")[1]
                 v = pretrained_dict[name]
                 all_params[k] = v
         assert len(all_params.keys()) == len(self.resnet.state_dict().keys())
